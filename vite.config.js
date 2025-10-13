1ad16cca27e3917c6c7a61704a95e430a81333b2
BHARGAVSAI558 <bhargavsai954@gmail.com>
2025-10-13 23:33:48 +0530

    upd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/frontent-gitactions',
+  base:'/practice-frontent-gitactions/',
 })
